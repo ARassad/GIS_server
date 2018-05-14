88b92bddd910ae3235b495e196b2aff682cebfaa
Mikhail <mikhailkovalyov34@gmail.com>
2018-05-14 13:18:38 +0300

    Исправлен метода возвращения всех точек

--- src/GetAllWayPoints.py
+++ src/GetAllWayPoints.py
@@ -5,9 +5,16 @@ class GetAllWayPoints(Request):
 
     @staticmethod
     def request(cursor, params, dataTransferObject):
-        cursor.execute("select * from point")
+        cursor.execute("select idFirstPoint, idSecondPoint from segmentStreet")
+        points = cursor.fetchall()
+        st = set()
+        for i in points:
+            st.add(i[0])
+            st.add(i[1])
+
         dataTransferObject.points = []
-        data = cursor.fetchall()
-        for i in data:
-            dataTransferObject.points.append({'xy': [i[1], i[2]], 'id': [i[0]]})
+        for i in st:
+            cursor.execute("select * from point where id={}".format(i))
+            data = cursor.fetchone()
+            dataTransferObject.points.append({'xy': [data[1], data[2]], 'id': [data[0]]})
         dataTransferObject.status = "OK"
